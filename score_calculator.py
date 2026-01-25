28a873e3ed0ab828775f9a0e99ed73d1cd05e86d
Utsav Dwivedi <utsavdwivedi51@gmail.com>
2026-01-25 09:45:04 +0530

    Delete score_calculator.py

--- score_calculator.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-def calculate_similarity(resume_text, jd_text):
-    texts = [resume_text, jd_text]
-    tfidf = TfidfVectorizer(stop_words='english')
-    matrix = tfidf.fit_transform(texts)
-    score = cosine_similarity(matrix[0:1], matrix[1:2])[0][0]
-    return round(score * 100, 2)
