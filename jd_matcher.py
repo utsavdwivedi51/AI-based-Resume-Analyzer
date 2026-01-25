b738f1a75a469c68810d6df54d13c8da31ed39e9
Utsav Dwivedi <utsavdwivedi51@gmail.com>
2026-01-25 09:54:54 +0530

    Delete jd_matcher.py

--- jd_matcher.py
+++
@@ -1,4 +0,0 @@
-def match_keywords(jd_keywords, resume_keywords):
-    matched = [word for word in jd_keywords if word in resume_keywords]
-    missing = [word for word in jd_keywords if word not in resume_keywords]
-    return matched, missing
